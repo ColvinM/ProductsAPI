7d3a4b366dbc4386fadd841d9ca7668eb3fa23b1
Maeghan Colvin <tazchicky2k@yahoo.com>
2024-03-06 15:01:40 -0500

    Add project files.

---
+++ Models/Product.cs
@@ -0,0 +1,13 @@
+﻿namespace ProductsAPI.Models
+{
+    public class Product
+    {
+        public long Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public double Price { get; set; }
+        public int UnitsInStock { get; set; }
+        public int UnitsOnOrder { get; set; }
+        public bool Discontinued { get; set; }
+    }
+}
